ff0231480dd13d11a274e561b8d813f3e1a52337
shtwangy <shtwangy@gmail.com>
2020-12-29 17:23:23 +0900

    feat(src/App.svelte): add computed property

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   export let counter = 0;
   const onClick = () => ++counter
+
+  $: doubled = counter * 2
 </script>
 
 <button on:click={onClick}>counter: {counter}</button>
+<span>double: {doubled}</span>
